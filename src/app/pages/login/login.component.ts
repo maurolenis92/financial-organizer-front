e1baf4c4d46b3fb2e275501d7668cad660fd0eff
Mauricio Lenis <mauricio.lenis@pragma.com.co>
2025-10-23 20:41:15 -0500

    form control added

--- src/app/pages/login/login.component.ts
+++ src/app/pages/login/login.component.ts
@@ -1,21 +1,36 @@
-import { Component, inject } from '@angular/core';
+import { Component, inject, OnInit } from '@angular/core';
 import { Router } from '@angular/router';
-import { FormsModule } from '@angular/forms';
+import {
+  FormBuilder,
+  FormControl,
+  FormGroup,
+  FormsModule,
+  ReactiveFormsModule,
+} from '@angular/forms';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-login',
   standalone: true,
-  imports: [FormsModule],
+  imports: [FormsModule, ReactiveFormsModule, CommonModule],
   templateUrl: './login.component.html',
   styleUrl: './login.component.scss',
 })
-export class LoginComponent {
+export class LoginComponent implements OnInit {
   public username = '';
   public password = '';
+  public form!: FormGroup;
   private router = inject(Router);
+  private fb = inject(FormBuilder);
 
-  public login(event: Event): void {
-    event.preventDefault();
+  ngOnInit(): void {
+    this.form = this.fb.group({
+      username: new FormControl(''),
+      password: new FormControl(''),
+    });
+  }
+
+  public login(): void {
     // Navegar al home cuando se presiona el botón
     this.router.navigate(['/home']);
   }
